ac9b3456894580d8180a3660101d82ea91cc17dc
itayeng <itayeng@gmail.com>
2013-07-08 00:01:34 +0300

    Revert "Basic Version"
    This reverts commit 64b0766abcfcd2f6f5ecd52a38f7feea1c92cfc3.

--- routes/admin/index.js
+++
@@ -1,8 +0,0 @@
-exports.index = function(req, res){
-  res.render('index', { title: 'admin.index' });
-};
-
-exports.logoff = function(req, res){
-  res.render('index', { title: 'admin.index.logoff' });
-};
-
